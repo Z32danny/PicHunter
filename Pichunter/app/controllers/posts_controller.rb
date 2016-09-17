53ec0efdfdd5e3ecf3defbfeb7a939fa38d66f51
Daniel Ruiz <danielruizgc8@yahoo.com>
2016-09-16 17:56:40 -0700

    forms for views, cleaned up code, more comment display stuff

--- Pichunter/app/controllers/posts_controller.rb
+++ Pichunter/app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
 
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+	before_action :owned_post, only: [:edit, :update, :destroy]
+
 	
 	def index
 		@posts = Post.all
@@ -65,6 +67,13 @@ class PostsController < ApplicationController
 
 		def set_post
 			@post = Post.find(params[:id])
+		end
+
+		def owned_post
+			unless current_user == @post.user
+				flash[:alert] = "Nice try slick, that post doesn't belong to you!"
+				redirect_to root_path
+			end
 		end
 
 
